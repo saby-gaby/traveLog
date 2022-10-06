7d2723fa9e26ffd2f4f795d34e9f83d13b8660d4
Tom Necke <92932124+Tom-Necke@users.noreply.github.com>
2022-10-06 21:12:22 +0200

    Update userLog-model.js

--- backend/models/userLog-model.js
+++ backend/models/userLog-model.js
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-const foodsSchema = new mongoose.Schema({
-  likes: [String],
-  dislikes: [String],
-  _id: false, // ohne _id:false fügt mir mongoose für dieses "Unterdokument" eine _id hinzu
-});
-
 const userLogSchema = new mongoose.Schema({
   name: {
     type: String,
